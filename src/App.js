4ba9f37548fb763c11ffdf36bbbb01b33f14c1dd
random-y94 <yaxenilafer@hotmail.com>
2022-11-09 17:30:22 -0500

    Componentes básicos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const title = 'welcome to the new blog'
-  const likes = 50; 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1> { title } </h1>
-        <p> Liked {likes} times</p>
+        <Home />
       </div>
     </div>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,13 @@
 const Navbar = () => {
-    return (  );
-}
- 
+  return (
+    <nav className="navbar">
+      <h1>The Shelly Diary</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Entry</a>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
