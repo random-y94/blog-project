e84eaf6f3e6d725f3c9371f31267a9f5caa024f8
random-y94 <yaxenilafer@hotmail.com>
2022-11-10 13:45:21 -0500

    fetching data de un server json local

--- src/Home.js
+++ src/Home.js
@@ -2,40 +2,21 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "JiU", id: 1 },
-    {
-      title: "Welcome party!",
-      body: "lorem ipsum...",
-      author: "Gahyeon",
-      id: 2,
-    },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "Handong",
-      id: 3,
-    },
-    { title: "My other website", body: "lorem ipsum...", author: "JiU", id: 4 },
-  ]);
-
-  const [name, setName] = useState("SuA");
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log("use effect ran");
-    console.log(name);
-  }, [name]);
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete} />
-      <button onClick={() => setName("Handong")}>change name</button>
-      <p>{name}</p>
+      {blogs && <BlogList blogs={blogs} title="All blogs!" />}
     </div>
   );
 };
