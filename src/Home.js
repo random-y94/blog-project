f9dd49adc8c69d724ae1e93055171767f9ad7dd9
random-y94 <yaxenilafer@hotmail.com>
2022-11-09 22:38:27 -0500

    Renderizar listas de data al componente Home + estilos

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,30 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("Yoohyeon");
-  const [age, setAge] = useState(25);
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "JiU", id: 1 },
+    {
+      title: "Welcome party!",
+      body: "lorem ipsum...",
+      author: "Gahyeon",
+      id: 2,
+    },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "Handong",
+      id: 3,
+    },
+  ]);
 
-  const handleClick = () => {
-    setName("Siyeon");
-    setAge(27);
-  };
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old.
-      </p>
-      <button onClick={handleClick}>click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
